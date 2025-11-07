e1c6427ade1c1550d1b204cdd03a70b7d78be317
seff-smith <sesmith1@s.sfusd.edu>
2025-11-07 11:35:52 -0800

    almost done with L5

---
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var rich_text_label: RichTextLabel = %RichTextLabel
+@onready var next_button: Button = %NextButton
+
+var dialogue_items: Array[String] = [
+	"I'm learning about Arrays...",
+	"...and it is a little bit complicated.",
+	"Let's see if I got it right: an array is a list of values!",
+	"Did I get it right? Did I?",
+	"Hehe! Bye bye~!",
+]
+
+var current_item_index := 0
+
+func _ready() -> void:
+	show_text()
+	next_button.pressed.connect(advance)
+
+func show_text() -> void:
+	var current_item := dialogue_items[current_item_index]
+	rich_text_label.text = current_item
+
+func advance() -> void:
+	current_item_index += 1
+	if current_item_index == dialogue_items.size():
+		get_tree().quit()
+	else:
+		show_text()
